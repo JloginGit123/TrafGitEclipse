845394f1dbcf938721fca5be3da9d60fd4166924
Jayashree K R <163240@CTSC00513942601.cts.com>
2017-10-04 10:38:56 +0530

    IssueID::UST131 OwnerId::2222255813 entityType::AUSRST Status::Done Reviewer::admin Comments::Commit by J

--- TrafJava/src/BusinessJava.java
+++ TrafJava/src/BusinessJava.java
@@ -4,6 +4,7 @@ public class BusinessJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("I am here!!!");//ust131;
+		 System.out.println("Code REcommit!!!");//ust131;
 	}
 
 }
